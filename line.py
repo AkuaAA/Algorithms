7db8418dd6dfc0a2f04536990798622f661c5784
Akua Apeagyei <apeagyei.akua@gmail.com>
2019-05-07 16:27:47 +0100

    added data to create graph using pyplot

--- line.py
+++ line.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 
-    x = [268, 609]
-    y = [0.0003647804260253906, 0.00010395050048828125]
-    plt.plot(x,y)
+    x = [268, 609, 366, 230, 895]
+    y = [0.0003647804260253906, 0.00010395050048828125, 0.0002181529998779297, 0.00023603439331054688, 0.00037789344787597656]
+    plt.plot(x,color='orange')
+    plt.plot(y, color='g')
+    plt.xlabel('size')
+    plt.ylabel('runtime')
+    plt.title('sort method')
     plt.show()
+
+x =
