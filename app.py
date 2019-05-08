624eba73f10af452545fa25cb9595b67cab8c105
Akua Apeagyei <apeagyei.akua@gmail.com>
2019-05-08 11:20:18 +0100

    Created shuffle algorithm and added json to visualise graph on browser

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 #Created a simple flask application
 
-from flask import Flask
+from flask import Flask, render_template,request
+from flask import jsonify
+import json
 import time
 import random
 import numpy
 import plotly
 from random import randint
 
-#created global variable
-global x_cord
-x_cord =[]
-
-global y_cord
-y_cord =[]
 
 app = Flask(__name__)
 
@@ -61,6 +57,27 @@ def shuffle_time():
     time_now = time.time()
     final_time = time_now - current_time
     return str(final_time) #converts into a string
+
+@app.route("/myshuffle")
+def my_shuffle():
+    co_ord = []
+    current_time = time.time()
+    for k in range (1,1001, +50): #creating array list
+        list = numpy.random.random_integers(1, 100, k) #generates random array
+        current_time = time.time()
+        print(list)
+        for i in range((len(list)-1), 0, -1): #for loop that specifies how many elements are actually in the array that you want to work on
+            a = random.randint(0, len(list)-1) #generates number
+            list[i],list[a] = list[a],list[i] # syntax for swapping the numbers
+        print(list) #print to visualise the code
+        time_now = time.time()
+        final_time = time_now - current_time
+        co_ord.append([k,final_time]) #creates the format of the x and y cordinates the way json likes it
+    return jsonify(co_ord) #created to visualise graph
+
+@app.route("/graph")
+def graph():
+    return render_template("graph.html")
 
 
 if __name__ == "__main__":
--- line.py
+++ line.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-
-    x = [268, 609, 366, 230, 895]
+#creating a graph structure
+    x = [268, 609, 366, 230, 895] #received data by running application on local machine several time
     y = [0.0003647804260253906, 0.00010395050048828125, 0.0002181529998779297, 0.00023603439331054688, 0.00037789344787597656]
     plt.plot(x,color='orange')
     plt.plot(y, color='g')
@@ -8,5 +8,3 @@ import matplotlib.pyplot as plt
     plt.ylabel('runtime')
     plt.title('sort method')
     plt.show()
-
-x =
